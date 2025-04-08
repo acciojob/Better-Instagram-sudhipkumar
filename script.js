cfdb32f100bcc14039d96a3ec780ea6bd7b31b11
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-04-08 19:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+let dragged = null;
+
+document.querySelectorAll('.image').forEach(image => {
+  image.addEventListener('dragstart', (e) => {
+    dragged = e.target;
+    e.target.classList.add('selected');
+  });
+
+  image.addEventListener('dragend', (e) => {
+    e.target.classList.remove('selected');
+  });
+
+  image.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  
+  });
+});
